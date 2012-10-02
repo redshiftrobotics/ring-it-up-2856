2eb61f06a6e201ff286ad911efe48d7e56c9f09f
Gabriel Cronin <gcronin@seattleacademy.org>
2012-10-02 09:51:32 -0700

    Changed deadband to 9.

--- simple_tank_contrl.c
+++ simple_tank_contrl.c
@@ -18,12 +18,12 @@ task main()
 		rightpower = joystick.joy1_y2;		//read joystick y values (both sticks)
 		leftpower = joystick.joy1_y1;
 
-		if(abs(rightpower) < 8)			//deadband with threshold of 8
+		if(abs(rightpower) < 9)			//deadband with threshold of 8
 			{
 					rightpower = 0;
 			}
 
-		if(abs(leftpower) < 8)			//deadband with threshold of 8
+		if(abs(leftpower) < 9)			//deadband with threshold of 8
 			{
 				leftpower = 0;
 			}
